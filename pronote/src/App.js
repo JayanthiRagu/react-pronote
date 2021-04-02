57ea0b1520d68d5635963eee1c5ce363d94bf20e
Jayanthi <jayanthi.ragu93@gmail.com>
2021-04-02 13:07:03 +0530

    'first_coommit'

---
+++ pronote/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Pronote from './components/Editor.jsx';
+
+function App() {
+  return (
+    <div className="App">
+      <Pronote/>
+    </div>
+  );
+}
+
+export default App;
